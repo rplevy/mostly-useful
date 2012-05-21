a0e6fa412feaa53e78d2019e46edb0d649f63473
Rob Levy <robert.levy@drakerlabs.com>
2012-05-21 16:17:42 -0400

    bump version for reduce-keepv change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mostly-useful "0.0.1"
+(defproject mostly-useful "0.1.0"
   :description "Mostly useful utility functions."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
