8bd2bb93ebdb44cc3238a20aa67f0ec1dc6c5531
Rob Levy <robert.levy@drakerlabs.com>
2012-06-22 17:02:16 -0400

    upgrade midje

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0-RC1"]]}}
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
